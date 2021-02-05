92b8fec29f0f7f89ee89c9a7c9819b0985a80480
Bhanu <bhanu3475@gmail.com>
2021-02-05 15:25:22 +0530

    minor fixes

--- bhanu.js
+++ bhanu.js
@@ -3,3 +3,7 @@ function remMarq(){
     document.getElementById("floatZq").remove()
 }
 close.addEventListener("click",remMarq)
+//<button class="border border-support-300  bg-white white radius100p block flex flex-column items-center justify-center" style="width: 40px; height: 40px;"><svg class="svg-icon__icon--thumbsup-outline icon-medium fill-support-500 transition-all transition-150 w100p" aria-hidden="false" aria-label="thumbsup"><use xlink:href="#icon--thumbsup-outline"></use></svg></button>
+
+
+
